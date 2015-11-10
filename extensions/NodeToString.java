46143961f9cf89089cb68a20fdf5af882b6bb050
mhe504 <mhe504@york.ac.uk>
2015-11-10 17:00:19 +0000

    Fixed COMMENT ON bug

--- extensions/NodeToString.java
+++ extensions/NodeToString.java
@@ -20,33 +20,33 @@ public class NodeToString extends MappingExtensionPoint {
 	public ExtensionValueReturn execute() {
 		
 		Object param0 =  getParam(0);
+		
 		if (param0 instanceof List<?>)
 		{
 			List<?> nodes = (List<?>) getParam(0);
 			if (nodes.get(0) instanceof Node)
 			{
 				Node column = (Node) nodes.get(0);
-				
 				String result = "";
-				for (Leaf l : column.getLeaves())
-					result = result + l.getValue();
+
 				if (getParams().size() ==2 )
 				{
+					result = column.getLeaves().get(2).getValue();
 					List<?> nodes2 = (List<?>) getParam(1);
 					Node name = (Node) nodes2.get(0);
-					if ("column_comment".equals(name.getKind()))
-					{
-						EList<Element> children = name.getChildren();
-						Node n3 = (Node)children.get(3);
-						Node n4 = (Node)children.get(1);
-						result = result + ";POSTPROCESS_MOVE:" + n4.getLeaves().get(0).getValue() + "." + 
-								n3.getLeaves().get(0).getValue() +",itemUnit;";
-					}
-					else
-					{
-						result = result + ";POSTPROCESS_MOVE:" + name.getLeaves().get(0).getValue() + ",dataElement, data:RelationalTable;";						
-					}
+					result = result.replace("'", "") + ";POSTPROCESS_MOVE:" + name.getLeaves().get(0).getValue() + ",dataElement, data:RelationalTable;";						
+
 				}
+				else
+				{
+					EList<Element> children =column.getChildren();
+					Node n3 = (Node)children.get(3);
+					Node n4 = (Node)children.get(1);
+					Leaf l = (Leaf)children.get(5);
+					result = l.getValue().replace("'", "") + ";POSTPROCESS_MOVE:" + n4.getLeaves().get(0).getValue() + "." + 
+							n3.getLeaves().get(0).getValue() +",itemUnit;";
+				}
+				
 				return returnString(result);
 			}
 		}
